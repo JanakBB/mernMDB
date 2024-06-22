c01d64c41bad389f29b21c5766ae7611490a65f6
Janak Bahadur Bohara <janakcustomx@gmail.com>
2024-06-22 17:43:37 +0545

    added mongoose to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const userRouter = require("./Router/userRouter");
 const postRouter = require("./Router/postRouter");
 const logger = require("./middleWare/logger");
 const errorHandlor = require("./middleWare/errorMiddleWare");
 
 const app = express();
+
+//connect mongodb
+(async () => {
+    try{
+        let connection = await mongoose.connect("mongodb://localhost:27017/Batch3");
+        console.log(`connecting mongoDB at ${connection.connection.host}`);
+        
+        app.listen(3300, () => {
+            console.log("Server 3300 is running......");
+        })
+    } catch(err) {
+        console.log("Error connect at", err.message);
+        process.exit(1);
+    }
+})();
 
 app.use(express.json());
 
@@ -14,6 +30,3 @@ app.use(express.static("public"));
 
 app.use(errorHandlor);
 
-app.listen(3300, () => {
-    console.log("Server 3300 is running......");
-})
